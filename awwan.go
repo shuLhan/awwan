006fe79d7523c47102462595d8e8664fc1910c6a
Shulhan <ms@kilabit.info>
2023-10-04 23:50:54 +0700

    Release awwan v0.8.0 (2023-10-04)
    This release add support for encryption, with two new commands "encrypt"
    and "decrypt" for encrypting and decrypting file with RSA private key.
    
    The awwan command also can read encrypted environment file with the name
    ".awwan.env.vault", so any secret variables can stored there and the
    script that contains '{{.Val "..."}}' works as usual.
    
    Any magic put "#put" also can copy encrypted file without any changes, as
    long as the source file with ".vault" extension exist.
    
    For environment where awwan need to be operated automatically, for example
    in build system, awwan can read the private key's passphrase automatically
    from the file ".ssh/awwan.pass".
    
    === Bug fixes
    
    * all: do not expand environment during parseScript
    * all: fix #require does not get executed on the same start
    * _www: fix execute request that still use "begin_at" and "end_at"
    
    === Enhancements
    
    * all: make .Vars, .Val, and .Vals panic if values is empty
    * all: print any path relative to base directory
    * all: move field bufout and buferr out of httpServer struct

--- awwan.go
+++ awwan.go
@@ -16,7 +16,7 @@ import (
 )
 
 // Version current version of this module (library and program).
-const Version = `0.7.0`
+const Version = `0.8.0`
 
 // List of command available for program awwan.
 const (
