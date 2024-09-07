1b3ca357ef8294fcf9a35d46cc2ea194b4b09dc2
Shulhan <ms@kilabit.info>
2024-09-08 01:09:18 +0700

    Release awwan v0.12.2 (2024-09-08)
    === Chores
    
    * all: replace licensing format to REUSE.toml
    
      Using ".reuse/dep5" has been deprecated since REUSE v3.2.0.
    
    * _wui: update wui submodule URL
    
      The wui submodule has been renamed to "pakakeh.ts".

--- awwan.go
+++ awwan.go
@@ -20,7 +20,7 @@ import (
 )
 
 // Version current version of this module (library and program).
-var Version = `0.12.1`
+var Version = `0.12.2`
 
 // osGetwd define the handler to get current working directory.
 //
